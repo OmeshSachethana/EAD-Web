973e8c9575f5f962c406044e6441a1e5c7715e02
Omesh Sachethana <sachethana12@gmail.com>
2024-09-22 03:08:09 +0530

    register page

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,14 @@
+// src/pages/RegisterPage.js
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+const RegisterPage = () => {
+  return (
+    <div className="container">
+      <h2>Register</h2>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
