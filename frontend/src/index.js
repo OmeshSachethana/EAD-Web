a45c5cf7707f11ab57f0d5ba8363cb4a390df8c6
Omesh Sachethana <sachethana12@gmail.com>
2024-09-22 03:10:30 +0530

    fixed issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './app/store';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+import { store } from './app/store';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap styling
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
+    <Router>
       <App />
-  </Provider>,
-  document.getElementById('root')
+    </Router>
+  </Provider>
 );
