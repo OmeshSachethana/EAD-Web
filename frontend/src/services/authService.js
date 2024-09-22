f720d1f89004091c27b4444149f942284b8cb3f7
Omesh Sachethana <sachethana12@gmail.com>
2024-09-22 07:02:07 +0530

    commented

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_URL = 'http://localhost:5207/api/users/';
+// const API_URL = 'http://localhost:5207/api/users/';
 
-// Login function
-const login = async (email, password) => {
-    const response = await axios.post(`${API_URL}login`, { email, password });
-    return response.data; // Expecting the token in response
-};
+// // Login function
+// const login = async (email, password) => {
+//     const response = await axios.post(`${API_URL}login`, { email, password });
+//     return response.data; // Expecting the token in response
+// };
 
-// Register function
-const register = async (username, email, password) => {
-    const response = await axios.post(API_URL, { username, email, password });
-    return response.data;
-};
+// // Register function
+// const register = async (username, email, password) => {
+//     const response = await axios.post(API_URL, { username, email, password });
+//     return response.data;
+// };
 
-const authService = {
-    login,
-    register,
-};
+// const authService = {
+//     login,
+//     register,
+// };
 
-export default authService;
+// export default authService;
