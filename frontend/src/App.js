ca591b1c8dc9ba3be92a9ba117be216c3b1be5cc
Omesh Sachethana <sachethana12@gmail.com>
2024-09-22 20:08:14 +0530

    added to protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,33 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Navbar from './components/Navbar'; // Ensure this import is correct
 import ProtectedRoute from './components/ProtectedRoute'; // Ensure this import is correct
+import ProductPage from './pages/ProductPage'; // Import the ProductPage
 
 const App = () => {
-  const { token } = useSelector((state) => state.auth);
+  const { token } = useSelector((state) => state.auth); // Get the authentication token
 
   return (
     <div>
       <Navbar />
       <Routes>
+        {/* Public routes */}
         <Route path="/login" element={token ? <Navigate to="/" /> : <LoginPage />} />
         <Route path="/register" element={token ? <Navigate to="/" /> : <RegisterPage />} />
+        
+        {/* Protected routes */}
         <Route
           path="/"
           element={
             <ProtectedRoute token={token}>
               <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute token={token}>
+              <ProductPage /> {/* ProductPage is only accessible if logged in */}
             </ProtectedRoute>
           }
         />
