c7f4c3e31b68ecc07f502548872774a549027696
Omesh Sachethana <sachethana12@gmail.com>
2024-09-22 20:31:41 +0530

    reversed

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
+// src/api/authApi.js
 import axios from 'axios';
 
-// Access the backend URL from the environment variables
-const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api/users`;
+const API_URL = 'http://localhost:5207/api/users';  // Your backend URL
 
 export const loginUser = async (credentials) => {
   const response = await axios.post(`${API_URL}/login`, credentials);
